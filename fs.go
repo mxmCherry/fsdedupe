63b408a1a641d0409cfc5eb20b1019a0e15716d5
mxmCherry <mxmCherry@gmail.com>
2023-07-30 17:40:48 +0300

    fs: chroot-like deduplicate file manager

---
+++ fs.go
@@ -0,0 +1,236 @@
+package fsdedupe
+
+import (
+	"crypto/sha512"
+	"errors"
+	"fmt"
+	"hash"
+	"io"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+// FS is a deduplicated files manager.
+// It keeps files in one dir by their content hash (SHA512),
+// and symlinks (with human-ish names) to them in another dir.
+type FS struct {
+	tempDir string
+	dataDir string
+	linkDir string
+	dirPerm os.FileMode
+}
+
+// NewFS constructs a new FS with given details.
+func NewFS(
+	tempDir string,
+	dataDir string,
+	linkDir string,
+	dirPerm os.FileMode,
+) (*FS, error) {
+	var err error
+
+	if filepath.IsLocal(tempDir) {
+		if tempDir, err = filepath.Abs(tempDir); err != nil {
+			return nil, fmt.Errorf("resolve abs path for tempDir: %w", err)
+		}
+	}
+	if filepath.IsLocal(dataDir) {
+		if dataDir, err = filepath.Abs(dataDir); err != nil {
+			return nil, fmt.Errorf("resolve abs path for dataDir: %w", err)
+		}
+	}
+
+	if filepath.IsLocal(linkDir) {
+		if linkDir, err = filepath.Abs(linkDir); err != nil {
+			return nil, fmt.Errorf("resolve abs path for linkDir: %w", err)
+		}
+	}
+
+	if dirPerm == 0 {
+		dirPerm = 0700
+	}
+
+	return &FS{
+		tempDir: tempDir,
+		dataDir: dataDir,
+		linkDir: linkDir,
+		dirPerm: dirPerm,
+	}, nil
+}
+
+// Create creates or truncates/opens existing file to be written by caller.
+func (s *FS) Create(linkName string) (io.WriteCloser, error) {
+	absLinkName := filepath.Join(
+		s.linkDir,
+		filepath.Join(string(filepath.Separator), linkName),
+	)
+	return createFile(s.tempDir, s.dataDir, absLinkName, s.dirPerm)
+}
+
+// Open opens the file for reading.
+func (s *FS) Open(linkName string) (io.ReadCloser, error) {
+	absLinkName := filepath.Join(
+		s.linkDir,
+		filepath.Join(string(filepath.Separator), linkName),
+	)
+	return os.Open(absLinkName)
+}
+
+// Rename renames (moves) the file.
+func (s *FS) Rename(oldLinkName, newLinkName string) error {
+	cleanOldLinkName := filepath.Join(string(filepath.Separator), oldLinkName)
+	absOldLinkName := filepath.Join(
+		s.linkDir,
+		cleanOldLinkName,
+	)
+	absNewLinkName := filepath.Join(
+		s.linkDir,
+		filepath.Join(string(filepath.Separator), newLinkName),
+	)
+
+	if err := os.MkdirAll(filepath.Dir(absNewLinkName), s.dirPerm); err != nil {
+		return fmt.Errorf("mkdir: %w", err)
+	}
+
+	if err := os.Rename(absOldLinkName, absNewLinkName); err != nil {
+		return fmt.Errorf("rename %q -> %q: %w", absOldLinkName, absNewLinkName, err)
+	}
+
+	if err := cleanTree(s.linkDir, filepath.Dir(cleanOldLinkName)); err != nil {
+		return fmt.Errorf("clean tree of %q: %w", cleanOldLinkName, err)
+	}
+	return nil
+}
+
+// Remove removes the file.
+func (s *FS) Remove(linkName string) error {
+	cleanLinkName := filepath.Join(string(filepath.Separator), linkName)
+	absLinkName := filepath.Join(
+		s.linkDir,
+		cleanLinkName,
+	)
+	if err := os.RemoveAll(absLinkName); err != nil {
+		return fmt.Errorf("rm: %w", err)
+	}
+
+	if err := cleanTree(s.linkDir, filepath.Dir(cleanLinkName)); err != nil {
+		return fmt.Errorf("clean tree: %w", err)
+	}
+	return nil
+}
+
+// GC removes unreferenced data files.
+func (s *FS) GC() error {
+	return errors.New("not implemented") // TODO: implement GC-ing
+}
+
+// ----------------------------------------------------------------------------
+
+func cleanTree(root, dir string) error {
+	for dir != string(filepath.Separator) {
+		absDir := filepath.Join(root, dir)
+
+		empty, err := isDirEmpty(absDir)
+		if err != nil {
+			return fmt.Errorf("check empty %q: %w", absDir, err)
+		}
+		if !empty {
+			return nil
+		}
+
+		if err := os.RemoveAll(absDir); err != nil {
+			return fmt.Errorf("rm %q: %w", absDir, err)
+		}
+
+		dir = filepath.Dir(dir)
+	}
+
+	return nil
+}
+
+func isDirEmpty(dir string) (bool, error) {
+	d, err := os.Open(dir)
+	if err != nil {
+		return false, fmt.Errorf("open: %w", err)
+	}
+	defer d.Close()
+
+	names, err := d.Readdirnames(1)
+	if errors.Is(err, io.EOF) {
+		return true, nil
+	} else if err != nil {
+		return false, err
+	}
+	return len(names) == 0, nil
+}
+
+// ----------------------------------------------------------------------------
+
+type fileWriter struct {
+	io.Writer
+
+	tempFileName string
+	dataDir      string
+	absLinkName  string
+	dirPerm      os.FileMode
+
+	tempFile *os.File
+	digest   hash.Hash
+}
+
+func createFile(tempDir, dataDir, absLinkName string, dirPerm os.FileMode) (*fileWriter, error) {
+	tempFileName := filepath.Join(tempDir, fmt.Sprintf("%d.bin", time.Now().UnixNano()))
+
+	if err := os.MkdirAll(filepath.Dir(tempFileName), dirPerm); err != nil {
+		return nil, fmt.Errorf("ensure dir for %q: %w", tempFileName, err)
+	}
+
+	tempFile, err := os.Create(tempFileName)
+	if err != nil {
+		return nil, fmt.Errorf("create temp file %q: %w", tempFileName, err)
+	}
+
+	digest := sha512.New()
+
+	return &fileWriter{
+		Writer: io.MultiWriter(tempFile, digest),
+
+		tempFileName: tempFileName,
+		dataDir:      dataDir,
+		absLinkName:  absLinkName,
+		dirPerm:      dirPerm,
+
+		tempFile: tempFile,
+		digest:   digest,
+	}, nil
+}
+
+func (f *fileWriter) Close() error {
+	if err := f.tempFile.Close(); err != nil {
+		return fmt.Errorf("close temp file %q: %w", f.tempFileName, err)
+	}
+
+	absDataName := filepath.Join(
+		f.dataDir,
+		fmt.Sprintf("%x", f.digest.Sum(nil))+".bin",
+	)
+
+	if err := os.MkdirAll(filepath.Dir(absDataName), f.dirPerm); err != nil {
+		return fmt.Errorf("ensure dir for %q: %w", absDataName, err)
+	}
+
+	if err := os.Rename(f.tempFileName, absDataName); err != nil {
+		return fmt.Errorf("rename temp file %q into data file %q: %w", f.tempFileName, absDataName, err)
+	}
+
+	if err := os.MkdirAll(filepath.Dir(f.absLinkName), f.dirPerm); err != nil {
+		return fmt.Errorf("ensure dir for %q: %w", f.absLinkName, err)
+	}
+
+	if err := os.Symlink(absDataName, f.absLinkName); err != nil {
+		return fmt.Errorf("symlink %q pointing to data file %q: %w", f.absLinkName, absDataName, err)
+	}
+
+	return nil
+}
---
+++ fs_test.go
@@ -0,0 +1,142 @@
+package fsdedupe_test
+
+import (
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/mxmCherry/fsdedupe"
+)
+
+func TestFS_Create(t *testing.T) {
+	tmp := t.TempDir()
+	subject := setupFS(t, tmp)
+
+	const name = "sub/dir/file.txt"
+	const contents = "DUMMY"
+	const contentsHash = "0a8649de6b948fac1722c82ee07f4e3e8386a071750daf23c56fbba31acc922323b362fe10327e7e3322bc9354df59e02ded56f7f6f0ebfd6e99702154299d51" // echo -n DUMMY | sha512sum
+
+	setupFS_Create(t, subject, name, contents)
+
+	absLinkPath := filepath.Join(tmp, "link", name)
+	b, err := ioutil.ReadFile(absLinkPath)
+	if err != nil {
+		t.Fatalf("expected no error, got: %s", err)
+	}
+	if actual, expected := string(b), "DUMMY"; err != nil {
+		t.Errorf("expected %q, got %q", actual, expected)
+	}
+
+	absDataPath := filepath.Join(tmp, "data", contentsHash+".bin")
+	if actual, expected := readlink(t, absLinkPath), absDataPath; actual != expected {
+		t.Errorf("expected link %q to point to point to content-hash-ed data file %q, got this instead: %q", contentsHash, expected, actual)
+	}
+}
+
+func TestFS_Read(t *testing.T) {
+	tmp := t.TempDir()
+	subject := setupFS(t, tmp)
+
+	const name = "sub/dir/file.txt"
+	const contents = "DUMMY"
+
+	setupFS_Create(t, subject, name, contents)
+
+	r, err := subject.Open(name)
+	if err != nil {
+		t.Fatalf("expected no error, got: %s", err)
+	}
+	defer r.Close()
+
+	b, err := io.ReadAll(r)
+	if err != nil {
+		t.Fatalf("expected no error, got: %s", err)
+	}
+
+	if actual, expected := string(b), contents; actual != expected {
+		t.Errorf("expected %q, got %q", expected, actual)
+	}
+}
+
+func TestFS_Rename(t *testing.T) {
+	tmp := t.TempDir()
+	subject := setupFS(t, tmp)
+
+	const oldName = "sub/dir/file.txt"
+	const newName = "another/sub/dir/file.txt"
+	const contents = "DUMMY"
+
+	setupFS_Create(t, subject, oldName, contents)
+
+	absOldName := filepath.Join(tmp, "link", oldName)
+	absNewName := filepath.Join(tmp, "link", newName)
+
+	oldLink := readlink(t, absOldName)
+
+	if err := subject.Rename(oldName, newName); err != nil {
+		t.Fatalf("expected no error, got: %s", err)
+	}
+
+	_, err := os.Stat(filepath.Join(tmp, "link", oldName))
+	if !os.IsNotExist(err) {
+		t.Errorf("expected old parent dir to be gone, but it still exists")
+	}
+
+	newLink := readlink(t, absNewName)
+	if actual, expected := newLink, oldLink; actual != expected {
+		t.Errorf("expected %q, got %q", expected, actual)
+	}
+}
+
+func TestFS_Remove(t *testing.T) {
+	tmp := t.TempDir()
+	subject := setupFS(t, tmp)
+
+	const name = "sub/dir/file.txt"
+	const contents = "DUMMY"
+
+	setupFS_Create(t, subject, name, contents)
+
+	if err := subject.Remove(name); err != nil {
+		t.Fatalf("expected no error, got: %s", err)
+	}
+
+	// this, obviously, asserts that file is gone as well
+	_, err := os.Stat(filepath.Join(tmp, "link", "sub"))
+	if !os.IsNotExist(err) {
+		t.Errorf("expected old parent dir to be gone, but it still exists")
+	}
+}
+
+// ----------------------------------------------------------------------------
+
+func setupFS(t *testing.T, tmp string) *fsdedupe.FS {
+	fs, err := fsdedupe.NewFS(
+		filepath.Join(tmp, "temp"),
+		filepath.Join(tmp, "data"),
+		filepath.Join(tmp, "link"),
+		0700,
+	)
+	if err != nil {
+		t.Fatalf("expected no error, got: %s", err)
+	}
+
+	return fs
+}
+
+func setupFS_Create(t *testing.T, fs *fsdedupe.FS, name, contents string) {
+	f, err := fs.Create(name)
+	if err != nil {
+		t.Fatalf("expected no error, got: %s", err)
+	}
+	defer f.Close()
+
+	if _, err := io.WriteString(f, contents); err != nil {
+		t.Fatalf("expected no error, got: %s", err)
+	}
+	if err := f.Close(); err != nil {
+		t.Fatalf("expected no error, got: %s", err)
+	}
+}
